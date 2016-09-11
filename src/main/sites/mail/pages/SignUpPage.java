fb21f7ed0bca980808bb34a9120f64d0a4fef01c
Leon Hunter <xleonhunter@gmail.com>
2016-09-11 08:49:11 -0400

    package update

--- src/main/sites/mail/pages/SignUpPage.java
+++ src/main/sites/mail/pages/SignUpPage.java
@@ -1,9 +1,6 @@
 package main.sites.mail.pages;
 
-import java.util.ArrayList;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 import main.sites.AbstractPageObject;
 import main.sites.AbstractTrial;
