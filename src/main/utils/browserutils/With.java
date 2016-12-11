e975e52c7379f612c98c5b7f7d28217120184998
Leon Hunter <xleonhunter@gmail.com>
2016-12-11 12:18:08 -0500

    Refined With.java

--- src/main/utils/browserutils/With.java
+++ src/main/utils/browserutils/With.java
@@ -2,28 +2,69 @@ package main.utils.browserutils;
 
 import org.openqa.selenium.By;
 
-public abstract class With extends By {
-	public static By attributeValueBeing(String attribute, String attributeValue) {
-		return By.xpath(String.format("//*[@s='%s')]", attribute, attributeValue));
-	}
+/**
+ * @purpose Mechanism to locate elements within a document
+ */
+public abstract class With {
 
-	public static By attributeValueHaving(String attribute, String attributeValue) {
-		return By.xpath(String.format("//*[contains(@s, '%s')]", attribute, attributeValue));
-	}
+    /**
+     * @param text The value of the "text" to search for
+     * @return a By which locates button elements by the value of the text
+     */
+    public static By buttonText(String text) {
+        return tagAndText("button", text);
+    }
 
-	public static By buttonTextHaving(String attribute, String text) {
-		return By.xpath(String.format("//button[contains(text(), '%s')]", text));
-	}
+    /**
+     * @param attribute the name of the attribute to search for
+     * @param value the value of the attribute to search for
+     * @return a By object which locates elements by the value of the attribute
+     */
+    public static By attributeValue(String attribute, String value) {
+        return By.xpath(String.format("(.//*[contains(@%s, '%s')])", attribute, value));
+    }
 
-	public static By classValueBeing(String classValue) {
-		return attributeValueBeing("class", classValue);
-	}
+    /**
+     * @param id the value of the "id" attribute to search for
+     * @return a By object which locates elements by the value of the attribute.
+     */
+    public static By attribute(String attribute) {
+        return By.cssSelector(String.format("[%s]"));
+    }
 
-	public static By classValueHaving(String classValue) {
-		return attributeValueHaving("class", classValue);
-	}
+    /**
+     * @param text The value of the "text" to search for
+     * @return a By which locates elements by the value of the text
+     */
+    public static By text(String text) {
+        return tagAndText("*", text);
+    }
 
-	public static By textHaving(String text) {
-		return By.xpath(String.format("//*[contains(text(), '%s')]", text));
-	}
+    /**
+     * @param tag The tag to search for
+     * @param text The text-value to search for
+     * @return a By which locates button elements by the tag and text-value
+     */
+    public static By tagAndText(String tag, String text) {
+        return By.xpath(String.format(".//%s[contains(text(), '%s')]", tag, text));
+    }
+
+    /**
+     * @param tag the tag to search for
+     * @param attribute the name of the attribute to search for
+     * @param value the value of the attribute to search for
+     * @return a By object which locates elements with specified tag and value of the attribute
+     */
+    public static By tagAttributeValue(String tag, String attribute, String value) {
+        return By.xpath(String.format(".//%s[@%s='%s']", tag, attribute, value));
+    }
+    
+    /**
+     * @param tag the tag to search for
+     * @param attribute the name of the attribute to search for
+     * @return a By object which locates elements with specified tag and attribute
+     */
+    public static By tagAttribute(String tag, String attribute) {
+        return By.xpath(String.format(".//%s['%s']", tag, attribute));
+    }
 }
